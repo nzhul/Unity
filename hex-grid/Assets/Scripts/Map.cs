af1c66648490f144c2072fa6baf9cf72f083e587
Dobromir Ivanov <dobromirivanov1@gmail.com>
2016-05-03 11:04:20 +0300

    Adding hex-grid project to the solution

---
+++ hex-grid/Assets/Scripts/Map.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexPrefab;
+
+	private int height = 20;
+	private int width = 20;
+
+	float xOffset = 0.882f;
+	float zOffset = 0.764f;
+
+	void Start () {
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				float xPos = x * xOffset;
+
+				if (y % 2 == 0)
+				{
+					xPos += xOffset / 2f;
+				}
+
+				GameObject hex_go = Instantiate(hexPrefab, new Vector3(xPos * 2, 0, y * zOffset * 2), Quaternion.identity) as GameObject;
+				hex_go.name = "Hex_" + x + "_" + y;
+
+				hex_go.transform.SetParent(this.transform);
+
+				hex_go.isStatic = true;
+			}
+		}
+	}
+
+}
---
+++ hex-grid/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	void Update()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast(ray, out hitInfo))
+		{
+			Debug.Log("Raycast hit: " + hitInfo.collider.transform.parent.name);
+
+			GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				MeshRenderer mr = ourHitObject.GetComponent<MeshRenderer>();
+
+				if (mr.material.color == Color.red)
+				{
+				mr.material.color = Color.white;
+				}
+				else
+				{
+					mr.material.color = Color.red;
+				}
+			}
+		}
+	}
+}
