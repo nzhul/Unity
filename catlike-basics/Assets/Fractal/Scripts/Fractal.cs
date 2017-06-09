62c22db63a96ae03c7d227d599af24a62f22507c
nzhul <dobromirivanov1@gmail.com>
2017-06-09 23:29:09 +0300

    finishing fractal tutorial from catlike coding

--- catlike-basics/Assets/Fractal/Scripts/Fractal.cs
+++ catlike-basics/Assets/Fractal/Scripts/Fractal.cs
@@ -2,22 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fractal : MonoBehaviour {
-
-	public Mesh mesh;
+public class Fractal : MonoBehaviour
+{
 	public Material material;
-	
+	private Material[,] materials;
+	public Mesh[] meshes;
+	public float spawnProbability;
+	public float maxRotationSpeed;
+	public float maxTwist;
+
 	public int maxDepth;
 	private int depth;
+	private float rotationSpeed;
 
 	private void Start()
 	{
-		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+		transform.Rotate(Random.Range(-maxTwist, maxTwist), 0f, 0f);
+
+		if (materials == null)
+		{
+			InitializeMaterials();
+		}
+
+		gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
 		gameObject.AddComponent<MeshRenderer>().material = material;
-		
-		if(depth < maxDepth){
+		GetComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
+
+		if (depth < maxDepth)
+		{
 			StartCoroutine(CreateChildren());
 		}
+	}
+
+	private void Update()
+	{
+		transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+	}
+
+	/// <summary>
+	/// InitializeMaterials is needed because of Dynamic Batching
+	/// If we do not initialize the materials on start, then new material will be created for every next item.
+	/// </summary>
+	private void InitializeMaterials()
+	{
+		materials = new Material[maxDepth + 1, 2];
+		for (int i = 0; i <= maxDepth; i++)
+		{
+			float t = i / (maxDepth - 1f);
+			t *= t;
+			materials[i, 0] = new Material(material);
+			materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+			materials[i, 1] = new Material(material);
+			materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+		}
+
+		materials[maxDepth, 0].color = Color.magenta;
+		materials[maxDepth, 1].color = Color.red;
 	}
 
 	private static Vector3[] childDirections = {
@@ -35,21 +76,28 @@ public class Fractal : MonoBehaviour {
 		Quaternion.Euler(90f, 0f, 0f),
 		Quaternion.Euler(-90f, 0f, 0f)
 	};
-	
-	private IEnumerator CreateChildren(){
-		for(int i = 0; i < childDirections.Length; i++){
-			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
-			new GameObject("Fractal Child")
-			.AddComponent<Fractal>()
-			.Initialize(this, i);
+
+	private IEnumerator CreateChildren()
+	{
+		for (int i = 0; i < childDirections.Length; i++)
+		{
+			if (Random.value < spawnProbability)
+			{
+				yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+				new GameObject("Fractal Child")
+				.AddComponent<Fractal>()
+				.Initialize(this, i);
+			}
 		}
 	}
 
-	
+
 	public float childScale;
-	
-	private void Initialize(Fractal parent, int childIndex){
-		mesh = parent.mesh;
+
+	private void Initialize(Fractal parent, int childIndex)
+	{
+		meshes = parent.meshes;
+		materials = parent.materials;
 		material = parent.material;
 		maxDepth = parent.maxDepth;
 		depth = parent.depth + 1;
@@ -58,5 +106,8 @@ public class Fractal : MonoBehaviour {
 		transform.localScale = Vector3.one * childScale;
 		transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
 		transform.localRotation = childOrientations[childIndex];
+		spawnProbability = parent.spawnProbability;
+		maxRotationSpeed = parent.maxRotationSpeed;
+		maxTwist = parent.maxTwist;
 	}
 }
