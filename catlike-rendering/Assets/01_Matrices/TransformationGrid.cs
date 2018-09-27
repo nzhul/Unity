d6ef3390bd9cc3128f6a2d3555cb54a7d81511f3
nzhul <dobromirivanov1@gmail.com>
2018-09-27 21:35:36 +0300

    starting catlike rendering section

---
+++ catlike-rendering/Assets/01_Matrices/TransformationGrid.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour
+{
+    Matrix4x4 transformation;
+
+    public Transform prefab;
+
+    public int gridResolution = 10;
+
+    Transform[] grid;
+
+    List<Transformation> transformations;
+
+    private void Awake()
+    {
+        grid = new Transform[gridResolution * gridResolution * gridResolution];
+        for (int i = 0, z = 0; z < gridResolution; z++)
+        {
+            for (int y = 0; y < gridResolution; y++)
+            {
+                for (int x = 0; x < gridResolution; x++, i++)
+                {
+                    grid[i] = CreateGridPoint(x, y, z);
+                }
+            }
+        }
+
+        transformations = new List<Transformation>();
+    }
+
+    private void Update()
+    {
+        UpdateTransformation();
+        for (int i = 0, z = 0; z < gridResolution; z++)
+        {
+            for (int y = 0; y < gridResolution; y++)
+            {
+                for (int x = 0; x < gridResolution; x++, i++)
+                {
+                    grid[i].localPosition = TransformPoint(x, y, z);
+                }
+            }
+        }
+    }
+
+    void UpdateTransformation()
+    {
+        GetComponents<Transformation>(transformations);
+        if (transformations.Count > 0)
+        {
+            transformation = transformations[0].Matrix;
+            for (int i = 1; i < transformations.Count; i++)
+            {
+                transformation = transformations[i].Matrix * transformation;
+            }
+        }
+    }
+
+    private Vector3 TransformPoint(int x, int y, int z)
+    {
+        Vector3 coordinates = GetCoordinates(x, y, z);
+        return transformation.MultiplyPoint(coordinates);
+    }
+
+    private Transform CreateGridPoint(int x, int y, int z)
+    {
+        Transform point = Instantiate<Transform>(prefab);
+        point.localPosition = GetCoordinates(x, y, z);
+        point.GetComponent<MeshRenderer>().material.color = new Color(
+            (float)x / gridResolution,
+            (float)y / gridResolution,
+            (float)z / gridResolution);
+
+        return point;
+    }
+
+    private Vector3 GetCoordinates(int x, int y, int z)
+    {
+        return new Vector3(
+            x - (gridResolution - 1) * .5f,
+            y - (gridResolution - 1) * .5f,
+            z - (gridResolution - 1) * .5f
+            );
+    }
+}
---
+++ catlike-rendering/Assets/02_Shaders/My First Shader.shader
@@ -0,0 +1,44 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/My First Shader" {
+
+
+
+Properties {
+	_Tint ("Tint", Color) = (1,1,1,1)
+}
+
+SubShader {
+
+	Pass {
+		CGPROGRAM
+
+		struct Interpolators {
+			float4 position : SV_POSITION;
+			float3 localPosition : TEXCOORD0;
+		};
+
+		#pragma vertex MyVertexProgram
+		#pragma fragment MyFragmentProgram
+
+		#include "UnityCG.cginc"
+
+		float4 _Tint;
+
+		Interpolators MyVertexProgram (float4 position : POSITION) {
+			Interpolators i;
+			i.localPosition = position.xyz;
+			i.position = UnityObjectToClipPos(position);
+			return i;
+		}
+
+		float4 MyFragmentProgram (Interpolators i) : SV_TARGET {
+				return float4(i.localPosition, 1);
+			}
+
+		ENDCG
+	}
+
+}
+
+}
