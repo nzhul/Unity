ccba3d1afdc7acbf04e7736d268c6f5f4b873226
nzhul <dobromirivanov1@gmail.com>
2019-09-26 21:43:52 +0300

    start catlike object management series

---
+++ catlike-object-management/Assets/Scripts/Game.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : PersistableObject
+{
+    const int saveVersion = 1;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    public ShapeFactory shapeFactory;
+
+    public PersistentStorage storage;
+
+    List<Shape> shapes;
+
+
+    private void Awake()
+    {
+        shapes = new List<Shape>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(createKey))
+        {
+            CreateShape();
+        }
+        else if (Input.GetKey(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            storage.Save(this, saveVersion);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(shapes.Count);
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            writer.Write(shapes[i].ShapeId);
+            writer.Write(shapes[i].MaterialId);
+            shapes[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int version = reader.Version;
+        if (version > saveVersion)
+        {
+            Debug.LogError("Unsupported future save version " + version);
+            return;
+        }
+        int count = version <= 0 ? -version : reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            int shapeId = version > 0 ? reader.ReadInt() : 0;
+            int materialId = version > 0 ? reader.ReadInt() : 0;
+            Shape instance = shapeFactory.Get(shapeId, materialId);
+            instance.Load(reader);
+            shapes.Add(instance);
+        }
+    }
+
+    private void BeginNewGame()
+    {
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            Destroy(shapes[i].gameObject);
+        }
+        shapes.Clear();
+    }
+
+    private void CreateShape()
+    {
+        Shape instance = shapeFactory.GetRandom();
+        Transform t = instance.transform;
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        instance.SetColor(Random.ColorHSV(
+                    hueMin: 0f, hueMax: 1f,
+                    saturationMin: 0.5f, saturationMax: 1f,
+                    valueMin: 0.25f, valueMax: 1f,
+                    alphaMin: 1f, alphaMax: 1f
+                ));
+        shapes.Add(instance);
+    }
+}
---
+++ catlike-object-management/Assets/Scripts/Shape.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class Shape : PersistableObject
+{
+    static int colorPropertyId = Shader.PropertyToID("_Color");
+
+    static MaterialPropertyBlock sharedPropertyBlock;
+
+    int shapeId = int.MinValue;
+
+    Color color;
+
+    MeshRenderer meshRenderer;
+
+    public int MaterialId { get; private set; }
+
+    public int ShapeId
+    {
+        get
+        {
+            return shapeId;
+        }
+        set
+        {
+            if (shapeId == int.MinValue && value != int.MinValue)
+            {
+                shapeId = value;
+            }
+            else
+            {
+                Debug.LogError("Not allowed to change shapeId.");
+            }
+        }
+    }
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.Write(color);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+    }
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        meshRenderer.material = material;
+        this.MaterialId = materialId;
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+
+        /// A downside of setting a material's color is that this results in the creation of a new material, 
+        /// unique to the shape. This happens each time its color is set. We can avoid this by using a MaterialPropertyBlock instead.
+        //meshRenderer.material.color = color;
+
+        if (sharedPropertyBlock == null)
+        {
+            sharedPropertyBlock = new MaterialPropertyBlock();
+        }
+
+        sharedPropertyBlock.SetColor(colorPropertyId, color);
+        meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+    }
+}
